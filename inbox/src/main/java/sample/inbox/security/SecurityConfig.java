bfbfe3397869513badc7a75edd446a98156aa525
Rob Winch <rwinch@users.noreply.github.com>
2018-09-15 22:03:11 -0500

    Use encoded password

--- inbox/src/main/java/sample/inbox/security/SecurityConfig.java
+++ inbox/src/main/java/sample/inbox/security/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
 	@Bean
 	MapReactiveUserDetailsService userDetailsService() {
 		UserDetails rob = User.withUsername("rob@example.com")
-				.password("password")
+				.password("{bcrypt}$2a$10$sU1NRy7TqnYiSAcbsO8/3OF3B2I6r4R/5m9YWvr.bE8gKxDo2UfB2")
 				.roles("USER")
 				.build();
 		return new MapReactiveUserDetailsService(rob);
